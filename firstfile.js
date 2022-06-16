23881c59eb09cf7e2582f6bea7d01242d05472d7
RITA  T DE BRITO FARIAS <ritaglf@gmail.com>
2022-06-16 20:21:48 -0300

    Update firstfile.js

--- firstfile.js
+++ firstfile.js
@@ -1,3 +1,9 @@
 alert("Minha primeira mensagem")
 confirm("Tá gostando de programar?")
-prompt("Qual é o seu nome?")
+var nome = prompt("Qual é o seu nome?")
+alert('é um grande prazer te conhecer, ' + nome + '!') // + é concatenar
+var n1 = Number(prompt('digite um numero: ')) 
+var n2 = Number(prompt('digite outro numero: ')) 
+var s  = n1 + n2
+alert('A soma dos valores é ' +s)
+
