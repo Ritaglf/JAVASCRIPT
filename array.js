df76eb428779df559c2b8931a27aa5159f8a0a00
RITA  T DE BRITO FARIAS <ritaglf@gmail.com>
2022-06-22 20:43:34 -0300

    inicio novos arquivos

---
+++ array.js
@@ -0,0 +1,16 @@
+// ARRAY = ARRANJO = VETOR
+
+// LISTA DE ITENS
+
+let produtos = ['calça', 'camisa', 'saia'];
+
+console.log(produtos);
+
+//ver em inspecionar
+
+// array cada item é separado por aspas
+
+// se fosse uma aspa no começo e outra no fim seria string
+
+
+
---
+++ index.js
@@ -0,0 +1,16 @@
+let nome = 'Ana';
+
+nome = 'Rita';
+
+let idade = '20';
+
+let cidade = 'São Paulo';
+
+concatenar = `A ${nome} tem ${idade} anos e mora da cidade de ${cidade}`
+
+
+
+console.log(concatenar)
+
+//ver em inspecionar
+
