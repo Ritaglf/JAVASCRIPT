5c114f61b435cebec966310ddf31a2a3ea9c5004
RITA  T DE BRITO FARIAS <ritaglf@gmail.com>
2022-06-22 17:32:17 -0300

    início arquivos

---
+++ funcao.js
@@ -0,0 +1,10 @@
+//tipo de dado de referencia
+
+//função = function
+
+//declaramos a função
+
+function exibirMensagem(primeiroNome,ultimoNome) {//parametro
+alert(`Olá ${primeiroNome} ${ultimoNome}, tudo bem?`);
+}
+exibirMensagem('Ana', 'Silva');
